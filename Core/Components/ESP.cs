bf40ca5ca67473397485054b39cdc10d8f2dc49d
y <11300364+MazeW@users.noreply.github.com>
2021-07-09 00:40:58 +0300

    esp fix
    fixed the shit valve broke in last update

--- Core/Components/ESP.cs
+++ Core/Components/ESP.cs
@@ -35,11 +35,11 @@ namespace ChristWare.Core.Components
 
             var color = entityTeamId != teamId ? configuration.Value.EnemyColor : configuration.Value.FriendlyColor;
 
-            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x4, color.R / 255F); // R
-            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x8, color.G / 255F); // G
-            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0xC, color.B / 255F); // B
-            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x10, 1F); // Alpha
-            Memory.Write<int>(processHandle, manager + glow * 0x38 + 0x24, 1); // Toggle
+            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x8, color.R / 255F); // R
+            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0xC, color.G / 255F); // G
+            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x10, color.B / 255F); // B
+            Memory.Write<float>(processHandle, manager + glow * 0x38 + 0x14, 1F); // Alpha
+            Memory.Write<int>(processHandle, manager + glow * 0x38 + 0x28, 1); // Toggle
         }
     }
 }
